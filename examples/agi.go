d87e6fa5a00b008cfd22a5310071bc662c495117
Stas Kobzar <stas.kobzar@clearlyip.com>
2021-02-04 19:49:45 -0500

    Update agi example

--- examples/agi.go
+++ examples/agi.go
@@ -8,63 +8,69 @@ package main
 
 import (
 	"fmt"
-	"github.com/staskobzar/goagi"
 	"log"
+	"os"
+
+	"github.com/staskobzar/goagi"
 )
 
 var (
 	tests, fail, pass int
 )
 
-func checkResult(err error) {
+func checkResult(err error, resp goagi.Response) {
 	tests++
 	if err != nil {
 		fail++
 	} else {
 		pass++
 	}
+	log.Printf("Response code:   %d", resp.Code())
+	log.Printf("Response value:  %s", resp.Value())
+	log.Printf("Response data:   %s", resp.Data())
+	log.Printf("Response endpos: %s", resp.EndPos())
 }
 
 func main() {
-	agi, err := goagi.NewAGI()
+	agi, err := goagi.New(os.Stdin, os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	verb := func(msg string, args ...interface{}) {
-		if err := agi.Verbose(fmt.Sprintf(msg, args...)); err != nil {
+		if _, err := agi.Verbose(fmt.Sprintf(msg, args...)); err != nil {
 			log.Fatalln(err)
 		}
 	}
 
 	verb("1.  Testing 'sendfile'...")
-	_, err = agi.StreamFile("beep", "", 0)
-	checkResult(err)
+	resp, err := agi.StreamFile("beep", "", 0)
+	checkResult(err, resp)
 
 	verb("2.  Testing 'sendtext'...")
-	err = agi.SendText("hello world")
-	checkResult(err)
+	resp, err = agi.SendText("hello world")
+	checkResult(err, resp)
 
 	verb("3.  Testing 'sendimage'...")
-	err = agi.SendImage("hello world")
-	checkResult(err)
+	resp, err = agi.SendImage("hello world")
+	checkResult(err, resp)
 
 	verb("4.  Testing 'saynumber'...")
-	err = agi.SayNumber("192837465", "")
-	checkResult(err)
+	resp, err = agi.SayNumber("192837465", "")
+	checkResult(err, resp)
 
 	verb("5.  Testing 'waitdtmf'...")
-	dig, err := agi.WaitForDigit(1000)
-	checkResult(err)
-	verb("Digit received: '%s'", dig)
+	resp, err = agi.WaitForDigit(1000)
+	checkResult(err, resp)
+	verb("Digit received: '%s'", resp.Value())
 
 	verb("6.  Testing 'record'...")
-	err = agi.RecordFile("testagi", "gsm", "1234", 3000, 0, false, 0)
-	checkResult(err)
+	resp, err = agi.RecordFile("testagi", "gsm", "1234", 3000, 0, false, 0)
+	checkResult(err, resp)
 
 	verb("6a.  Testing 'record' playback...")
-	_, err = agi.StreamFile("testagi", "", 0)
-	checkResult(err)
+	resp, err = agi.StreamFile("testagi", "", 0)
+	checkResult(err, resp)
 
 	verb("================== Complete ======================")
 	verb("%d tests completed, %d passed, %d failed", tests, pass, fail)
